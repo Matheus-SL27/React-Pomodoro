7cf65ff6c64756c927ef498a3f1472a1ea581f42
Matheus-SL27 <matheus.silvalima2708@gmail.com>
2026-08-09 20:58:14 -0300

    primeiro heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import './styles/theme.css'
 import './styles/global.css'
 import { Heading } from './components/Heading'
+import { TimerIcon } from 'lucide-react'
 
 export function App(){
     return (
         <div>
-            <h1>Ola mundo</h1>
-            <Heading/>
+            <Heading>
+                Olá Mundo!
+                <button>
+                    <TimerIcon/>
+                </button>
+            </Heading>
             <p>Nossa main recebe nossos componentes com o export, não podemos fazer mais de uma linha html sem o ()</p>
         </div>
     )
