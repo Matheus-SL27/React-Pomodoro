b9b266e820c88776432a2998bd8d5bc2fba76548
Matheus-SL27 <matheus.silvalima2708@gmail.com>
2026-08-08 21:09:56 -0300

    Css definido

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-export function App(){
-    console.log("Rodando...")
+import './styles/theme.css'
+import './styles/global.css'
+import { Heading } from './components/Heading'
 
+export function App(){
     return (
         <div>
-            <h1>Aprendendo React</h1>
+            <h1>Ola mundo</h1>
+            <Heading/>
             <p>Nossa main recebe nossos componentes com o export, não podemos fazer mais de uma linha html sem o ()</p>
         </div>
     )
