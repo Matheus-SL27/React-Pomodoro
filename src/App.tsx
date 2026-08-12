dada9bde538c6bf143432dcedbc9e8fb06210374
Matheus-SL27 <matheus.silvalima2708@gmail.com>
2026-08-12 13:39:19 -0300

    designe menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
+import { Container } from './components/Container'
+import { Heading } from './components/Heading'
+import { Logo } from './components/Logo'
+import { Menu } from './components/Menu'
+
 import './styles/theme.css'
 import './styles/global.css'
-import { Heading } from './components/Heading'
-import { TimerIcon } from 'lucide-react'
+
 
 export function App(){
     return (
-        <div>
-            <Heading>
-                Olá Mundo!
-                <button>
-                    <TimerIcon/>
-                </button>
-            </Heading>
-            <p>Nossa main recebe nossos componentes com o export, não podemos fazer mais de uma linha html sem o ()</p>
-        </div>
+        <>
+            <Container>
+                <Logo/>
+            </Container>
+            <Container>
+                <Menu/>
+            </Container>
+        </>
     )
 }
